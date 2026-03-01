95c7662c6113dc544a29e3abd72248bb98460ab5
kalimuthu4978 <kalimuthu4978@gmail.com>
2026-03-01 11:13:00 +0530

    useState , sample like/dislike or +/- button to get the count of and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import ParentComponent from "./components/ParentComponent"
-type Props = {
-  name :string
-}
+import { useState } from "react";
+
 const App = () => {
-  const name :string = "Kali";
+
+  const [count, setCount] = useState(1);
+
+
+  const likeClick =() => {
+    if (count< 10) {
+     setCount(count + 1);
+    }
+
+  }
+
+    const dislikeClick =() => {
+      if (count >1) {
+            setCount(count - 1);
+      }
+  }
+
+
   return (
     <>
-      <ParentComponent name = {name}/>
+     
+      <button onClick={likeClick}> + </button>
+      &nbsp;&nbsp;   <span><strong>{count}</strong></span>  &nbsp;&nbsp; 
+
+      <button onClick={dislikeClick}> - </button>
+
+      &nbsp;&nbsp;
+      <button onClick={() => setCount(1)}> reset </button>
     </>
   )
 }
